4e30dd16cc7e9fd65bd5ca7cb33e77ea9881a80a
ruan <ruan.cavcalcante505@gmail.com>
2025-04-28 14:42:55 -0300

    feat: requisição HTTP

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        // receber username como argumento
+        System.out.print("Seu username: ");
+        String username = sc.nextLine();
+
+        // fazer requisição da API do github
+        final String URL_GET = "https://api.github.com/users/" + username + "/events";
+
+        // cliente http
+        HttpClient client = HttpClient.newHttpClient();
+        // criando a requisição
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .timeout(Duration.ofSeconds(10))
+                .uri(URI.create(URL_GET))
+                .build();
+        // enviando
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+        sc.close();
     }
 }
